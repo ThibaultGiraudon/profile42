cd5d86550eb96f8ea0bb267555f4bb22957a909a
ThibaultGiraudon <thibault.giraudon@gmail.com>
2024-09-16 10:55:02 +0200

    Initial Commit

---
+++ profile42/profile42App.swift
@@ -0,0 +1,17 @@
+//
+//  profile42App.swift
+//  profile42
+//
+//  Created by Thibault Giraudon on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct profile42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
