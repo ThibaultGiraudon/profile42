ec24bd1764457852d89bce19fb08a19534740be1
ThibaultGiraudon <thibault.giraudon@gmail.com>
2024-09-16 14:08:49 +0200

    remove api key

--- profile42/ContentView.swift
+++ profile42/ContentView.swift
@@ -8,15 +8,6 @@
 import SwiftUI
 import WebKit
 
-struct Constants {
-    static let authorizationApiUrl = "https://api.intra.42.fr/oauth/authorize"
-    static let clientID = "u-s4t2ud-1ed84540bd1267e075ee676f586b51a1d2c311c5e29f82473e31c50cef31b732"
-    static let clientSecret = "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"
-    static let tokenAPIURL  = "https://api.intra.42.fr/oauth/token"
-    static let redirectURI = "https://www.google.com/"
-    static let scopes = "public+projects+profile"
-}
-
 struct ContentView: View {
     private var authManager = AuthManager()
     @State private var showingWebView = false
